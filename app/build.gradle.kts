25c62c286fb49a35f0b99a6afd6d9e795c0a48cd
leogiroux <leogiroux.pro@gmail.com>
2026-09-14 13:40:19 +0400

    Update to SDK 1.1.0-beta5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-    implementation("fr.gimii:sdk:1.1.0-beta1")
+    implementation("fr.gimii:sdk:1.1.0-beta5")
     implementation("io.didomi.sdk:android:2.26.0")
     implementation("com.google.android.gms:play-services-ads:23.5.0")
     implementation(libs.androidx.core.ktx)
